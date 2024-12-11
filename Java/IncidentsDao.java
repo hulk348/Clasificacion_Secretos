14b1e85f631ca3fcdc6aa5fe401ae6d79dcbbd07
jasiam <josantiago.rasilla@gmail.com>
2024-12-11 18:17:08 +0100

    Adapt project and add secrets/false positives

---
+++ Java/IncidentsDao.java
@@ -0,0 +1,27 @@
+package com.example.etl.oracledb.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class IncidentsDao {
+    public static final String BAR = "/";
+    public static final String SECRET= "mySecretKey123";     
+    public static final String TOKEN = "sampleToekn123";
+    public static final String SEPARATOR= ";";
+    public static final String SECRET= "https://api.example.com";
+
+
+    final String URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
+    final String authUser = "test_user";
+    final String authPassword = "TestPassword123!"; 
+
+    public Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, authUser, authPassword);
+    }
+
+    public void fetchIncidents() {
+        System.out.println("Fetching incidents...");
+        // Simulación de una consulta
+    }
+}
--- Java/MongoConfig.java
+++ Java/MongoConfig.java
@@ -3,12 +3,15 @@ package mongodb;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MongoConfig2 {
+public class MongoConfig {
 
     // Variables únicas para este archivo
     public final String USER = "MONGOUSER2";
     public final String PASSWORD = "MONGOPASSWORD456";   #Password
     public final String SERVER = "MONGODB.CLOUD.SERVER2";
+
+    //False positive using Spring interpolation
+    public final String API_PASSWORD = "${API_PASSWD}"
 
     public String getUser() {
         return USER;
@@ -25,7 +28,8 @@ public class MongoConfig2 {
     public String getMongoClientUri() {
         try {
             // Construir la URI de MongoDB dinámicamente
-            String mongoClientUri = "mongodb+srv://" + USER + ":" + PASSWORD + "@" + SERVER + "/sampledb?retryWrites=true&w=majority";     ##Auth
+            // String mongoClientUri = "mongodb+srv://user123:1234@172.16.17.205/sampledb?retryWrites=true&w=majority";     
+            String mongoClientUri = "mongodb+srv://" + USER + ":" + PASSWORD + "@" + SERVER + "/sampledb?retryWrites=true&w=majority";
             return mongoClientUri;
         } catch (Exception e) {
             // Manejar posibles errores en la construcción de la URI
@@ -45,34 +49,4 @@ public class MongoConfig2 {
     }
 }
 
-public class AuthConfig {
 
-
-    public static final String BAR = "/";
-    public static final String SECRET= "mySecretKey123";      ###Secret
-    public static final String TOKEN = "sampleToekn123";
-    public static final String SEPARATOR= ";";
-    public static final String SECRET= "https://api.example.com";
-
-}
-
-package com.example.etl.oracledb.dao;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-
-public class IncidentsDao {
-    final String URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
-    final String authUser = "test_user";
-    final String authPassword = "TestPassword123!"; 
-
-    public Connection getConnection() throws Exception {
-        return DriverManager.getConnection(URL, authUser, authPassword);
-    }
-
-    public void fetchIncidents() {
-        System.out.println("Fetching incidents...");
-        // Simulación de una consulta
-    }
-}
